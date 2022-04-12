3b312712b79fbfce10e14c1a5e28c287026687e1
Nnam Darlington <Darlingtonnnam@gmail.com>
2022-04-12 04:24:31 +0100

    Create storage.sol

---
+++ storage.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ */
+contract Storage {
+
+    uint256 number;
+
+    // store unsigned integer in the number variable
+    function storeNumber(uint256 num) public {
+        number = num;
+    }
+
+    // retrieve the value of number
+    function retrieveNumber() public view returns (uint256){
+        return number;
+    }
+}
